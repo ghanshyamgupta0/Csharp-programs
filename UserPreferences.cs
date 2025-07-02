98aa924a9a49254e70b9fe6339bdb0e2b21c4434
ghanshyamgupta0 <guptahero056@gmail.com>
2025-07-02 09:04:21 +0545

    Adding Program files

--- UserPreferences.cs
+++ UserPreferences.cs
@@ -1,30 +1,9 @@
 class UserPreferences
 {
-    public void user(int age, double height, bool notifications)
+    public void User(int age, double height, bool notifications)
     {
         
         Console.WriteLine($"Age: {age}, Height: {height}, Notifications: {notifications}");
 
-    }
-    
-    static void Main(string[] args)
-    {
-        // Create a new instance of the UserPreferences class
-        var userPreferences = new UserPreferences();
-        
-        // Prompt the user for their preferences
-        Console.WriteLine("Setting user preferences...");
-        Console.WriteLine("Enter your age:");
-        int age = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter your height in meters:");
-        double height = double.Parse(Console.ReadLine());
-        Console.WriteLine("Do you want notifications? (true/false):");
-        bool notifications = bool.Parse(Console.ReadLine());
-
-        // Call the user method to set preferences
-        userPreferences.user(age, height, notifications);
-        
-        // Print a message indicating the end of the program
-        Console.WriteLine("User preferences set successfully.");
     }
 }

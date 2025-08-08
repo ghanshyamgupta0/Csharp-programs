cfe9851063c5fca081a642875f444923c0cb1795
ghanshyamgupta0 <guptahero056@gmail.com>
2025-08-07 22:58:10 -0700

    This is the calculator app for calculating your problems

--- Calculator.cs
+++ Calculator.cs
@@ -11,8 +11,8 @@ public class Calculator
         double num2 = Convert.ToDouble(Console.ReadLine());
 
         int i = 1;
-        while (i <= 5)
-        {
+        // while (i <= 5)
+        // {
             Console.WriteLine("Enter an operator (+, -, *, /, %):");
             char op = Convert.ToChar(Console.ReadLine());
             double add = num1 + num2;
@@ -31,8 +31,33 @@ public class Calculator
             };
 
             Console.WriteLine($"Result = {result}");
-            i++;
-        }
+            // i++;
+        // }
         Console.WriteLine("Thanks for using my Calculator App");
+
     }
+    
+    public void RunProgram() {
+        bool isAgain = true;
+        while (isAgain) {
+            Console.WriteLine("1. CalculateAgain \n2. Exit");
+            int choose = Convert.ToInt32(Console.ReadLine());
+
+            switch (choose)
+            {
+                case 1:
+                    Calculate();
+                    break;
+
+                case 2:
+                    isAgain = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option selected.");
+                    break;
+            }
+        }
+    }
+    
 }

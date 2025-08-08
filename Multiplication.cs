ccd50c9cb01faeeef2fc5432ce88b7186acfc1f5
ghanshyamgupta0 <guptahero056@gmail.com>
2025-08-07 23:13:36 -0700

    Adding Multiplication Table

---
+++ Multiplication.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Multiplication{
+    public void Multiply () {
+        isMultiply = true;
+        while (isMultiply) {
+            Console.WriteLine("Enter the number you want multiplication table:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            for (i = 1; i<=10; i++) {
+                int result = number * i;
+                Console.WriteLine($"{number} X {i} = {result}");
+            }
+            isMultiply = false;
+        }
+    }
+}

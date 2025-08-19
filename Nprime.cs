2977371fc60fa44dcc0b2b034d7549e540ebf46f
ghanshyamgupta0 <guptahero056@gmail.com>
2025-08-19 22:21:37 +0545

    This Program finds n number of prime numbers.

---
+++ Nprime.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Nprime
+{
+    public void FindPrime()
+    {
+        Console.WriteLine("Enter how many prime numbers you want to find:");
+        int count = Convert.ToInt32(Console.ReadLine());
+        if (count <= 0)
+        {
+            Console.WriteLine("Please enter a positive integer.");
+            return;
+        }
+        int found = 0;
+
+        for (int num = 2; found <= count; num++)
+        {
+            bool isPrime = true;
+            for (int i = 2; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine(num);
+                found++;
+            }
+            if (found >= count)
+            {
+                break;
+            }
+        }
+    }
+}
